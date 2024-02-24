f28f9c42fbbaea100d98b0ae809485fbc78db723
oSABERo <zaqsf159359@gmail.com>
2024-02-24 15:49:47 +0700

    update

--- test_newton.py
+++ test_newton.py
@@ -4,5 +4,5 @@ def test_newton():
     f = lambda x: x**2 - 4
     df = lambda x: 2*x
     nm = Newton(f, df)
-    assert nm.solve(10, 1e-10, 100) == 2
-    assert nm.solve(-10, 1e-10, 100) == -2
+    assert round(nm.solve(10, 1e-10, 100), 10) == 2
+    assert round(nm.solve(-10, 1e-10, 100), 10) == -2
